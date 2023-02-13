4417bef515baa1a79e9341348054e141a7a136a0
rainbowpieeee <catherinestolbova@yandex.ru>
2023-02-13 16:18:45 +0300

    добавлены user.entity, wish.entity, offer.entity, wishlist.entity

--- src/offers/entities/offer.entity.ts
+++ src/offers/entities/offer.entity.ts
@@ -1,1 +1,40 @@
-export class Offer {}
+import { User } from 'src/users/entities/user.entity';
+import { Wish } from 'src/wishes/entities/wish.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Offer {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  //user содержит id желающего скинуться;
+  @ManyToOne(() => User, (user) => user.id)
+  user: User;
+
+  //item содержит ссылку на товар;
+  @ManyToOne(() => Wish, (wish) => wish.id)
+  item: Wish;
+
+  //amount — сумма заявки, округляется до двух знаков после запятой;
+  @Column()
+  amount: number;
+
+  //hidden — флаг, который определяет показывать ли информацию о скидывающемся в списке. По умолчанию равен false.
+  @Column({
+    default: false,
+  })
+  hidden: boolean;
+}
--- src/wishlists/entities/wishlist.entity.ts
+++ src/wishlists/entities/wishlist.entity.ts
@@ -1,1 +1,42 @@
-export class Wishlist {}
+import { IsUrl, Length } from 'class-validator';
+import { User } from 'src/users/entities/user.entity';
+
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Wishlist {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  //name — название списка. Не может быть длиннее 250 символов и короче одного;
+  @Column()
+  @Length(1, 250)
+  name: string;
+
+  //description — описание подборки, строка до 1500 символов;
+  @Column()
+  @Length(1, 1500)
+  description: string;
+
+  //image — обложка для подборки;
+  @Column()
+  @IsUrl()
+  image: string;
+
+  //owner - привязка к владельцу
+  @ManyToOne(() => User, (user) => user.id)
+  owner: User;
+}
